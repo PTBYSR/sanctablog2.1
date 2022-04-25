7c2d832a869bfbdb3dcc75f9e27b574b6420e67b
PTBYSR <paul.emechebe@gmail.com>
2022-04-25 21:35:59 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import AllNews from './Pages/AllNews';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/AllNews.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AllNews() {
+  return (
+    <div>AllNews</div>
+  )
+}
+
+export default AllNews
