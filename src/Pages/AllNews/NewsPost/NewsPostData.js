9fa5aa2d87058e47370c47c46fe49add1b43132a
PTBYSR <paul.emechebe@gmail.com>
2022-04-28 23:08:23 +0100

    chore

--- src/Pages/AllNews/NewsPost/NewsPostData.js
+++ src/Pages/AllNews/NewsPost/NewsPostData.js
@@ -1,16 +1,19 @@
 export const NewsPostData = [
     {
         id: 1,
+        image: 'image',
         title: "Heading 1",
         description: "this is the news 1 description",
     },
     {
         id: 2,
+        image: 'image',
         title: "Heading 2",
         description: "this is the news 2 description",
     },
     {
         id: 3,
+        image: 'image',
         title: "Heading 3",
         description: "this is the news 3 description",
     },
