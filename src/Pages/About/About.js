f6ef9cefc3d5f962b0ce94cb72b6c113581b8a66
PTBYSR <paul.emechebe@gmail.com>
2022-05-03 12:49:33 +0100

    about-page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,11 +3,14 @@ import {
   AboutSection
 
 } from './About.element'
+// import { ReactComponent as SIluminated } from './s-illuminated.png';
+import SIluminated from './s-illuminated.png'
 
 function About() {
   return (
     <AboutSection>
     About
+    <img src={SIluminated} />
     </AboutSection>
   )
 }
